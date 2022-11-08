f6c220fdfa45ce0319d67bf791b6985826e5feb5
AnnaKonkova <112868442+AnnaKonkova@users.noreply.github.com>
2022-11-09 00:18:23 +0300

    Add files via upload
    7,1 and 7,2

---
+++ Задание_7/7,2.py
@@ -0,0 +1,13 @@
+# -- coding: utf-8 --
+mass = []
+n = int(input('Введите колличкство элементов в массиве '))
+for i in range(n):
+    mass.append(int(input('Введите элемент ')))
+diff_mass = []
+for i in mass:
+    if i % 2 == 0 and i < 10:
+        diff_mass.append(i)
+if len(diff_mass) == 0:
+    print("Четных чисел в данном массиве нет.")
+else:
+    print(sorted(diff_mass))
