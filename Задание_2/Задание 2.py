afba6cd6e945aef4599ae01ae2b56488a9dd0677
AnnaKonkova <112868442+AnnaKonkova@users.noreply.github.com>
2022-11-20 15:16:22 +0300

    Push (1-8) using git gui

---
+++ Задание_2/Задание 2.py
@@ -0,0 +1,10 @@
+# -- coding: utf-8 --
+# # (10в)
+# import math
+#
+# x = 3.251
+# y = 0.325
+# z = 0.466 * 10 ** (-4)
+# s = math.pow(y, math.pow(math.fabs(x), 1 / 3)) + (math.pow(math.cos(y), 1 / 3))*(math.fabs(x-y)*(1 + ((math.pow(math.sin(z), 1 / 2)) / (math.sqrt(x+y))))) / math.exp(math.fabs(x - y)) + x / 2
+# print(s)
+#
