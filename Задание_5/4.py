0f6451934720a1ac2756caa2494700fe8ebc9d52
AnnaKonkova <112868442+AnnaKonkova@users.noreply.github.com>
2022-11-20 23:11:36 +0300

    another fix

--- Задание_5/4.py
+++
@@ -1,9 +0,0 @@
-# -- coding: utf-8 --
-x = int(input())
-y = int(input())
-day = 1
-while x < y:
-    x *= 1.1
-    day += 1
-print(day)
-
--- задание_4/7.py
+++
@@ -1,8 +0,0 @@
-# -- coding: utf-8 --
-col = int(input('enter col='))
-cum_fact = 0
-fact = 1
-for i in range(1, col + 1):
-    fact *= i
-    cum_fact += fact
-print(cum_fact)
