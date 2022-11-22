91b063f72286bd6ec39ad85b4fcd62d022c4315d
AnnaKonkova <112868442+AnnaKonkova@users.noreply.github.com>
2022-11-22 22:43:50 +0300

    corrected version 8_1

--- Task_8/Задание_8.1.py
+++ Task_8/Задание_8.1.py
@@ -1,13 +1,6 @@
 # -- coding: utf-8 --
+def FindTwins(n):
+    print([[i, i + 2] for i in range(n, 2 * n - 1)])
 
-def main(n):
-    for i in range(2,int((n**0.5)+1)):
-        if n%i==0:
-            return(False)
-        else:
-            return (True)
-def Blizn(n):
-    return main(n) and main(n-2) or main(n+2)
 
-n = int(input())
-print(*filter(Blizn, [x for x in range(n, 2*n+1)]))
+FindTwins(int(input()))
