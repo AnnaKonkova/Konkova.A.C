0a8cc6e959b896b6d6d12f370de5a8e388efe994
AnnaKonkova <112868442+AnnaKonkova@users.noreply.github.com>
2022-12-07 21:28:48 +0300

    with the change def

--- Task_7/7,2.py
+++ Task_7/7,2.py
@@ -1,13 +1,15 @@
 # -- coding: utf-8 --
-mass = []
-n = int(input('Введите колличкство элементов в массиве '))
-for i in range(n):
-    mass.append(int(input('Введите элемент ')))
-diff_mass = []
-for i in mass:
-    if i % 2 == 0 and i < 10:
-        diff_mass.append(i)
-if len(diff_mass) == 0:
-    print("Четных чисел в данном массиве нет.")
-else:
-    print(sorted(diff_mass))
+def mass_even_numb():
+    mass = []
+    n = int(input('Введите колличкство элементов в массиве '))
+    for i in range(n):
+        mass.append(int(input('Введите элемент ')))
+    diff_mass = []
+    for i in mass:
+        if i % 2 == 0 and i < 10:
+            diff_mass.append(i)
+    if len(diff_mass) == 0:
+        print("Четных чисел в данном массиве нет.")
+    else:
+        print(sorted(diff_mass))
+mass_even_numb()
